e6dd1bc02b0c88b1a2ff3de2c7374085b0a0c2aa
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-05-29 17:44:38 +0530

    Change Logout button css

--- frontend/src/component/Sidebar/Sidebar.jsx
+++ frontend/src/component/Sidebar/Sidebar.jsx
@@ -45,7 +45,15 @@ const Sidebar = ({ onLogout }) => {
           <ListItemText>Product</ListItemText>
         </ListItem>
         <div style={{ height: "200px" }} />
-        <ListItem onClick={handleLogout} button className="listItem">
+        <ListItem
+          onClick={handleLogout}
+          button
+          // className="listItem"
+          style={{
+            color: "rgb(179, 76, 76)",
+            boxShadow: "rgba(0, 0, 0, 0.1) 2px 2px 8px",
+          }}
+        >
           <ListItemText>Logout</ListItemText>
         </ListItem>
       </List>
