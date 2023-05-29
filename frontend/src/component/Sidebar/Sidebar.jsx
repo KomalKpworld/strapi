814672c4b8b86e2bba265e6095acf756c0096f0a
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-05-29 16:57:05 +0530

    Add css on Listitem in  SideBar Component

--- frontend/src/component/Sidebar/Sidebar.jsx
+++ frontend/src/component/Sidebar/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { List, ListItem, ListItemText, ListSubheader } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import "./Sidebar.css";
 
 const Sidebar = ({ onLogout }) => {
   const navigate = useNavigate();
+
   const handleLogout = () => {
     // Call the onLogout function passed from the parent component
     if (typeof onLogout === "function") {
@@ -14,20 +16,36 @@ const Sidebar = ({ onLogout }) => {
     <>
       <List>
         <ListSubheader>COLLECTION TYPES</ListSubheader>
-        <ListItem onClick={() => navigate("/homepage")} button>
+        <ListItem
+          onClick={() => navigate("/homepage")}
+          button
+          className="listItem"
+        >
           <ListItemText>HomePage</ListItemText>
         </ListItem>
-        <ListItem onClick={() => navigate("/category")} button>
+        <ListItem
+          onClick={() => navigate("/category")}
+          button
+          className="listItem"
+        >
           <ListItemText>Categories</ListItemText>
         </ListItem>
-        <ListItem onClick={() => navigate("/subcategory")} button>
+        <ListItem
+          onClick={() => navigate("/subcategory")}
+          button
+          className="listItem"
+        >
           <ListItemText>Sub-Categories</ListItemText>
         </ListItem>
-        <ListItem onClick={() => navigate("/product")} button>
+        <ListItem
+          onClick={() => navigate("/product")}
+          button
+          className="listItem"
+        >
           <ListItemText>Product</ListItemText>
         </ListItem>
-        <div style={{ height: "200px" }}/>
-        <ListItem onClick={handleLogout} button>
+        <div style={{ height: "200px" }} />
+        <ListItem onClick={handleLogout} button className="listItem">
           <ListItemText>Logout</ListItemText>
         </ListItem>
       </List>
