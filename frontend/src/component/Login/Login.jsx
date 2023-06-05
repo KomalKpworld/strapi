6e92eb45b378b47d39ada8cda78ae9c378747e0c
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-06-05 11:57:14 +0530

    Add Create Account functionality

--- frontend/src/component/Login/Login.jsx
+++ frontend/src/component/Login/Login.jsx
@@ -10,12 +10,14 @@ import {
   Container,
   Group,
   Button,
+  Modal,
 } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import HomeApp from "../HomeApp";
 
 const Login = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const loggedIn = localStorage.getItem("isLoggedIn") === "true";
@@ -65,6 +67,9 @@ const Login = () => {
   const handleLogout = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("currentUser");
+    localStorage.removeItem("token");
+    localStorage.clear();
   };
 
   if (isLoggedIn) {
@@ -74,6 +79,54 @@ const Login = () => {
       </BrowserRouter>
     );
   }
+
+  const handleModalSubmit = async (event) => {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const role = event.target.role.value;
+
+    const data = new FormData();
+    data.append("username", username);
+    data.append("email", email);
+    data.append("password", password);
+    data.append("role", role);
+
+    // Log the form data
+    for (const [key, value] of data.entries()) {
+      console.log(`${key}: ${value}`);
+    }
+
+    try {
+      const response = await fetch("http://localhost:1337/api/users", {
+        method: "POST",
+        body: data,
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(JSON.stringify(responseData));
+        // Handle success
+      } else {
+        const errorData = await response.json();
+        console.log(JSON.stringify(errorData));
+        // Handle error
+      }
+    } catch (error) {
+      console.log(error);
+      // Handle error
+    }
+  };
+
+  const handleCreateAccount = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -89,7 +142,7 @@ const Login = () => {
         </Title>
         <Text color="dimmed" size="sm" align="center" mt={5}>
           Do not have an account yet?{" "}
-          <Anchor size="sm" component="button">
+          <Anchor size="sm" component="button" onClick={handleCreateAccount}>
             Create account
           </Anchor>
         </Text>
@@ -121,6 +174,35 @@ const Login = () => {
           </form>
         </Paper>
       </Container>
+
+      <Modal
+        opened={isModalOpen}
+        onClose={handleModalClose}
+        title="Create an Account"
+        size="sm"
+      >
+        <form onSubmit={handleModalSubmit}>
+          <TextInput name="username" label="Username" required />
+          <TextInput name="email" type="email" label="Email" required />
+          <TextInput
+            name="password"
+            type="password"
+            label="Password"
+            required
+          />
+          <TextInput name="role" type="role" label="role" required />
+          <br />
+
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button type="button" onClick={handleModalClose} variant="outline">
+              Cancel
+            </Button>
+            <Button type="submit" style={{ marginLeft: "8px" }}>
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </Modal>
     </>
   );
 };
