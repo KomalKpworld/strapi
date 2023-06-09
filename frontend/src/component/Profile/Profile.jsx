3a7591f5a2e3a0321fdfb60faf885aef9f60b083
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-06-09 15:51:55 +0530

    Add Delete My Account functionality

--- frontend/src/component/Profile/Profile.jsx
+++ frontend/src/component/Profile/Profile.jsx
@@ -8,10 +8,17 @@ import {
   Col,
   Modal,
   message,
+  Card,
+  Space,
 } from "antd";
 import BootstrapLoader from "../BootstrapLoader";
-import { changeUserPassword } from "../../api";
+import { changeUserPassword, deleteUserData } from "../../api";
 import { useNavigate } from "react-router-dom";
+import {
+  DeleteOutlined,
+  KeyOutlined,
+  LeftCircleOutlined,
+} from "@ant-design/icons";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -22,6 +29,8 @@ const Profile = () => {
   const [form] = Form.useForm();
   const [isLoading, setIsLoading] = useState(false);
   const [changePassword, setChangePassword] = useState({});
+  const [deleteFormData, setDeleteFormData] = useState({});
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -90,22 +99,54 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteForm = async (values, event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      console.log("Form values:", values.id);
+
+      const success = await deleteUserData(values.id, token);
+
+      if (success) {
+        console.log("Deleting an entry");
+        message.success("Deleting an entry");
+        setIsDeleteModalOpen(false);
+        form.resetFields();
+        localStorage.clear();
+        window.location.reload();
+      } else {
+        console.log("Failed to delete entry.");
+        message.error("Failed to delete entry.");
+      }
+    } catch (err) {
+      console.log("Error:", err);
+      if (err.response) {
+        console.log("Error response:", err.response);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   if (!userData) {
     return <div>Loading...</div>;
   }
   const handleModalCancel = () => {
     form.resetFields();
     setIsModalOpen(false);
+    setIsDeleteModalOpen(false);
   };
+
+  const handleDelete = (record) => {
+    console.log("Select for Delete", record);
+    setDeleteFormData({ ...record });
+    setIsDeleteModalOpen(true);
+  };
+
   return (
     <>
-      <h5 style={{ textAlign: "center" }}>Your Info</h5>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <Card style={{ width: 500, margin: "0 auto" }}>
+        <h5 style={{ textAlign: "center" }}>Your Info</h5>
         <Form initialValues={userData} layout="vertical">
           <Row gutter={32}>
             <Col span={24}>
@@ -127,52 +168,65 @@ const Profile = () => {
             </Col>
             <Col span={24}></Col>
           </Row>
+          <Space>
+            <AntButton
+              type="text"
+              danger
+              onClick={handleProfile}
+              icon={<KeyOutlined />}
+              style={{
+                color: "#25805b",
+                backgroundColor: "#def7ec",
+                border: "1px solid #d1f3cd",
+                height: "auto",
+                padding: "10px",
+                borderRadius: "13px",
+                display: "flex",
+                alignItems: "baseline",
+                fontWeight: "700",
+              }}
+            >
+              Change Password
+            </AntButton>
+            <AntButton
+              type="text"
+              danger
+              icon={<DeleteOutlined />}
+              onClick={() => handleDelete(userData.currentUser)}
+              style={{
+                color: "#b34c4c",
+                backgroundColor: "#f6cccc",
+                border: "1px solid rgb(231 176 176)",
+                height: "auto",
+                padding: "10px",
+                borderRadius: "13px",
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "700",
+              }}
+            >
+              Delete My Account
+            </AntButton>
+            <AntButton
+              onClick={() => navigate(-1)}
+              icon={<LeftCircleOutlined />}
+              style={{
+                color: "#5250e6",
+                backgroundColor: "#f0f0ff",
+                border: "1px solid #ddddfe",
+                height: "auto",
+                padding: "10px 20px",
+                borderRadius: "13px",
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "700",
+              }}
+            >
+              Back
+            </AntButton>
+          </Space>
         </Form>
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          gap: "15px",
-          justifyContent: "center",
-        }}
-      >
-        <AntButton
-          mt="xl"
-          onClick={() => navigate(-1)}
-          style={{
-            color: "#5250e6",
-            backgroundColor: "#f0f0ff",
-            border: "1px solid #ddddfe",
-            height: "auto",
-            padding: "10px 20px",
-            borderRadius: "15px",
-            display: "flex",
-            alignItems: "center",
-            fontWeight: "700",
-          }}
-        >
-          Back
-        </AntButton>
-        <AntButton
-          type="text"
-          danger
-          onClick={handleProfile}
-          style={{
-            color: "#5250e6",
-            backgroundColor: "#f0f0ff",
-            border: "1px solid #ddddfe",
-            height: "auto",
-            padding: "10px 20px",
-            borderRadius: "15px",
-            display: "flex",
-            alignItems: "center",
-            fontWeight: "700",
-          }}
-        >
-          Apply to Change Password
-        </AntButton>
-      </div>
+      </Card>
 
       <Modal
         style={{ width: "400px" }}
@@ -252,6 +306,65 @@ const Profile = () => {
               >
                 <Input.Password />
               </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Modal>
+
+      <Modal
+        title="Delete Modal"
+        onOk={handleDeleteForm}
+        open={isDeleteModalOpen}
+        onCancel={handleModalCancel}
+        centered
+        footer={[
+          <div>
+            <Button
+              key="cancel"
+              onClick={handleModalCancel}
+              style={{
+                border: "2px solid #d1d5db",
+                backgroundColor: "transparent",
+                color: "black",
+                borderRadius: "10px",
+              }}
+            >
+              Cancel
+            </Button>{" "}
+            <Button
+              key="submit"
+              type="primary"
+              disabled={isLoading}
+              onClick={(event) =>
+                handleDeleteForm(form.getFieldsValue(), event)
+              }
+              style={{
+                borderRadius: "10px",
+              }}
+            >
+              Delete
+              {isLoading && <BootstrapLoader />}
+            </Button>
+          </div>,
+        ]}
+      >
+        <Form
+          form={form}
+          onFinish={handleDeleteForm}
+          initialValues={{ ...deleteFormData }}
+          layout="vertical"
+        >
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item label="ID" name="id" hidden>
+                <Input disabled />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={24}>
+              <b>{deleteFormData.username}</b> ! Are u sure for delete this
+              Account ?
             </Col>
           </Row>
         </Form>
